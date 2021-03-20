1215b82dbd6391ae7e2eac8c86265aa2cd31ffa6
Jose Tommaso <tommaso_j@hotmail.com>
2021-03-20 12:17:26 +0000

    npm emotion - styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,24 @@
+import styled from '@emotion/styled';
+
+const ContainerHeader = styled.header`
+    background-color: #26c6da;
+    padding: 10px;
+    font-weight: bold;
+    color: #fff;
+`;
+
+const TextHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
+
 const Header = ({title}) => {
     return ( 
-        <header>
-            <h1>{title}</h1>
-        </header>
+        <ContainerHeader>
+            <TextHeader>{title}</TextHeader>
+        </ContainerHeader>
      );
 }
  
